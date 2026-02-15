8c0c4eff7722196025078e9a021202455eade16e
KOMATISATYA <125573949+KOMATISATYA@users.noreply.github.com>
2026-02-15 13:09:03 +0530

    Delete final directory

--- final/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-GROQ_API_KEY1 = os.getenv("GROQ_API_KEY1")
-GROQ_API_KEY2 = os.getenv("GROQ_API_KEY2")
-
-API_KEY = os.getenv("HONEYPOT_API_KEY", "SECRET_KEY")
