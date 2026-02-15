7f30061d03723022d14eda8f65337c82e457c9fe
KOMATISATYA <125573949+KOMATISATYA@users.noreply.github.com>
2026-02-15 09:40:54 +0530

    Add files via upload

---
+++ final/agents/classifier_agent.py
@@ -0,0 +1,14 @@
+from autogen_agentchat.agents import AssistantAgent
+from final.model_client import get_model_client
+
+classifier_agent = AssistantAgent(
+    name="Classifier",
+    model_client=get_model_client(),
+    system_message="""
+Return JSON:
+{
+ "scamDetected": true/false,
+ "confidenceScore": float
+}
+"""
+)
